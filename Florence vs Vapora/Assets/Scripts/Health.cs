5ada6d8edd9cca60faaa5f3b315930887d8cde86
J4CK1007 <162834554+J4CK1007@users.noreply.github.com>
2024-06-27 13:58:17 +0800

    Made damage and heal function for the player health and added a health bar

--- Florence vs Vapora/Assets/Scripts/Health.cs
+++ Florence vs Vapora/Assets/Scripts/Health.cs
@@ -7,22 +7,46 @@ public class Health : MonoBehaviour
     
     [SerializeField] private int maxHealth;
     [SerializeField] private int health;
+    public HealthBar healthBar;
 
     void Start()
     {
         //Initalize health to max
         health = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
     }
 
-    public void TakeDamage()
+    //For debuggin purpose press P to damage player and O to heal player
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Heal(1);
+        }
+    }
+
+    public void TakeDamage(int damage)
     {
         //If the health is less than 0, die
         if (health <= 0) { Die(); }
+        //Set current health after taking damage
+        health -= damage;
+        healthBar.setHealth(health);
     }
 
-    public void Heal()
+    public void Heal(int heal)
     {
-
+        health += heal;
+        //If the health is greater than the max health, set current health to max health
+        if(health < maxHealth)
+        {
+            health = maxHealth;
+        }
+        healthBar.setHealth(health);
     }
 
     public void Die()
